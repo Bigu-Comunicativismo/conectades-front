b530d749b765850b36f9f90175a6cfce411013c1
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-09-29 16:36:51 -0300

    feat:update Paragraph component to receive specific css improving reusability

--- src/components/structuralComponents/Paragraph/index.tsx
+++ src/components/structuralComponents/Paragraph/index.tsx
@@ -5,11 +5,12 @@ interface ParagraphProps {
   size: 'sm' | 'md' | 'lg';
   weight?: 'regular' | 'medium' | 'semibold' | 'bold';
   variant?: 'primary' | 'secondary';
+  classCss?: string;
 }
 
-export function Paragraph({text, size, weight = 'regular', variant = 'primary'}: ParagraphProps) {
+export function Paragraph({text, size, weight = 'regular', variant = 'primary', classCss}: ParagraphProps) {
   return (
-    <p className={`${styles.paragraph} ${styles[size]} ${styles[weight]} ${styles[variant]}`}>
+    <p className={`${styles.paragraph} ${styles[size]} ${styles[weight]} ${styles[variant]} ${classCss}`}>
       {text}
     </p>
   );
