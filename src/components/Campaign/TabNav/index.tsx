503a14c57ab41137d5ad80b31d9e3c1951dbc245
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-07 02:23:39 -0300

    feature(tabnav):edit component to receive a object as a props to mapping and render items on received data's based

--- src/components/Campaign/TabNav/index.tsx
+++ src/components/Campaign/TabNav/index.tsx
@@ -2,15 +2,19 @@ import { useTabContext } from "@/contexts/campaign";
 import { Container } from "@/components/structuralComponents/Container";
 import styles from './TabNav.module.css';
 
-export function TabNav() {
+type Tab = {
+    id: number,
+    name: 'Sobre' | 'Organização' | 'Apoios' | 'Atualizações'
+}
+
+export function TabNav({tabList}: {tabList: Tab[]}) {
     const {activeItem, setActiveItem} = useTabContext();
     return (
         <Container classCss=''>
             <ul className={styles.navigationList}>
-                <li className={`${styles.navigationListItem} ${ activeItem === 'Sobre' && styles.navigationListItemActive}`} aria-label="Aba Sobre" onClick={() => setActiveItem('Sobre')}>Sobre</li>
-                <li className={`${styles.navigationListItem} ${ activeItem === 'Organização' && styles.navigationListItemActive}`} aria-label="Aba Organização" onClick={() => setActiveItem('Organização')}>Organização</li>
-                <li className={`${styles.navigationListItem} ${ activeItem === 'Apoios' && styles.navigationListItemActive}`} aria-label="Aba Apoios" onClick={() => setActiveItem('Apoios')}>Apoios</li>
-                <li className={`${styles.navigationListItem} ${ activeItem === 'Atualizações' && styles.navigationListItemActive}`} aria-label="Aba Atualizações" onClick={() => setActiveItem('Atualizações')}>Atualizações</li>
+                {tabList.map((tab) => {
+                    return <li className={`${styles.navigationListItem} ${activeItem === tab.name && styles.navigationListItemActive}`} aria-label={`Aba ${tab}`} onClick={() => setActiveItem(tab.name)} key={tab.id}>{tab.name}</li>
+                })}
             </ul>
         </Container>
     )
