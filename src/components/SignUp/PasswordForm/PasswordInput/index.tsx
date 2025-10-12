5dc61a5ed13e2a0ff7adda90d3e8d47edf6860d4
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-12 16:54:53 -0300

    feat:create password input personalized component

---
+++ src/components/SignUp/PasswordForm/PasswordInput/index.tsx
@@ -0,0 +1,50 @@
+import { Input } from "@/components/base/input/input"
+import { Container } from "../../../structuralComponents/Container"
+import { useState } from "react"
+import { Eye, EyeOff, Passcode } from "@untitledui/icons";
+import styles from "./PasswordInput.module.css"
+
+interface PasswordInputProps {
+    inputLabel: string;
+    inputPlaceholder: string;
+    inputValue: string;
+    setInputValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export function PasswordInput ({inputLabel, inputPlaceholder, inputValue, setInputValue}:PasswordInputProps) {
+
+    const [passwordType, setPasswordType] = useState('password');
+    const [isShowPassword, setIsShowPassword] = useState(false);
+    
+
+    return (
+        <Container classCss='flex relative'>
+                <Input 
+                    label={inputLabel} 
+                    type={ passwordType } 
+                    icon={ Passcode } 
+                    placeholder={inputPlaceholder}
+                    value={inputValue}
+                    onChange={(inputValue) => {
+                       setInputValue(inputValue);
+                       }} 
+                    className={styles.input}
+                    />
+                    {isShowPassword ? 
+                    <div className={`${styles.eyeIcon}`} 
+                    onClick={() => {
+                        setIsShowPassword(!isShowPassword)
+                        setPasswordType('password')}}>
+                        <Eye/>
+                    </div>
+                    :
+                    <div className={`${styles.eyeIcon}`} 
+                    onClick={() => {
+                        setIsShowPassword(!isShowPassword)
+                        setPasswordType('text')}}>
+                        <EyeOff/>
+                    </div>
+                    }
+            </Container>
+    )
+}
