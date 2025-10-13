f772b424f0484431983bfa54e0c23a8f0849924f
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-12 21:33:15 -0300

    refactor:change title and paragraph to formDescription component

--- src/components/SignUp/PasswordForm/index.tsx
+++ src/components/SignUp/PasswordForm/index.tsx
@@ -1,5 +1,3 @@
-import { Title } from "@/components/structuralComponents/Title";
-import { Paragraph } from "@/components/structuralComponents/Paragraph";
 import { PasswordInput } from "./PasswordInput";
 import { Button } from "@/components/base/buttons/button";
 import { SpanText } from "@/components/structuralComponents/SpanText";
@@ -7,22 +5,15 @@ import styles from "./PasswordForm.module.css";
 import { Container } from "@/components/structuralComponents/Container";
 import { validations } from "@/utils/validations";
 import { useState } from "react";
+import { FormDescription } from "../FormDescription";
 
 export function PasswordForm() {
     const [inputPasswordValue, setInputPasswordValue] = useState('');
     const [inputConfirmPasswordValue, setInputConfirmPasswordValue] = useState('');
     const {passwordHasUpperandLower, passwordHasNumber,  passwordHasMinChar}: {passwordHasUpperandLower: boolean, passwordHasNumber: boolean,  passwordHasMinChar: boolean} = validations.password(inputPasswordValue)
     return (
-        <form className={styles.formContainer}>
-            <Container classCss={styles.container}>
-                <Title.Level1 text="Crie uma senha" 
-                classCss={styles.title}/>
-                <Paragraph text="Estamos terminando! Por último, crie sua senha, ela será utilizada para você acessar seu perfil" 
-                size="sm" 
-                weight="regular" 
-                variant="secondary" 
-                classCss={styles.paragraph}/>
-            </Container>
+        <form >
+            <FormDescription titleText="Crie uma senha" paragraphText="Estamos terminando! Por último, crie sua senha, ela será utilizada para você acessar seu perfil"/>
             <Container classCss={styles.inputContainer}>
                 <Container classCss={styles.inputContainer}>
                     <PasswordInput inputLabel="Nova senha" inputPlaceholder="Senha" inputValue={inputPasswordValue} setInputValue={setInputPasswordValue}/>
