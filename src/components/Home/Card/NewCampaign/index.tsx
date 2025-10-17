4ed61d8845f2cb4e9b73fe12a0103913402b73ae
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-17 04:24:59 -0300

    style:use css class on span card title

--- src/components/Home/Card/NewCampaign/index.tsx
+++ src/components/Home/Card/NewCampaign/index.tsx
@@ -35,7 +35,7 @@ export function NewCampaignCard({cardList}: {cardList: CardProps[]}) {
             <Card.InforContainer>
               <Badge className={styles.badge} color="purple">{card.cardTag}</Badge>
               <Card.InforContainer cardGap='gap4'>
-                <SpanText text={card.cardName}  />
+                <SpanText text={card.cardName} classCss={styles.cardTitle} />
                 <Card.IconTextContainer>
                   <Avatar size='xs' src={card.cardAuthor.authorImage} alt={`Avatar do usuário ${card.cardAuthor.authorName}`} className={styles.avatar} />
                   <Card.Author name={card.cardAuthor.authorName} />
