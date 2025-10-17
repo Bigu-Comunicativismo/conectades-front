c67bcf79c65d9af88bb55ef10541fa67d360a5bf
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-17 02:22:19 -0300

    style:add lint exception on pin-input untitled component

--- src/components/base/pin-input/pin-input.tsx
+++ src/components/base/pin-input/pin-input.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import type { ComponentPropsWithRef } from "react";
 import { createContext, useContext, useId } from "react";
 import { OTPInput, OTPInputContext } from "input-otp";
