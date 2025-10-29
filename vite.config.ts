138f77b22e693b6a7896847530f1094f61e04bb3
ÍCARO VICTOR OLIVEIRA DOS SANTOS <victor.icarus@gmail.com>
2025-10-29 11:11:02 -0300

    chore:configure tanstack router plugin in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: "./src/pages",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+      routeFileIgnorePrefix: "-",
+      quoteStyle: "single"
+    }),
+    react(),
   ],
   server: {
     proxy: {
